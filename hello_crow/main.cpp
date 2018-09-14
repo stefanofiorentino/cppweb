1e69886fee77421a69c8dce03698d08ce3655e83
Stefano Fiorentino <stefano.fiore84@gmail.com>
2018-09-14 14:33:51 +0200

    First commit

---
+++ hello_crow/main.cpp
@@ -0,0 +1,19 @@
+#include "crow_all.h"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  crow::SimpleApp app;
+
+  CROW_ROUTE(app, "/")
+  ([](){
+    return "<div><h1>hello, stefano</h1></div>";
+  });
+
+  char* port = getenv("PORT");
+  uint16_t iPort = static_cast<uint16_t>(port != NULL ? stoi(port) : 18080);
+  cout << "PORT = " << iPort << endl;
+
+  app.port(iPort).multithreaded().run();
+  return 0;
+}
